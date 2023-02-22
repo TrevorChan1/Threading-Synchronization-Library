b095a477fc07421852ba39e93344e23bdac320a4
Trevor Chan <trevchan@bu.edu>
2023-02-21 20:48:49 -0500

    Made finalized first draft of pthread_create

--- threads.c
+++ threads.c
@@ -46,34 +46,33 @@ struct thread_control_block {
 	struct thread_control_block * nextThread;
 };
 
-struct thread_control_block *TCBTable[MAX_THREADS];
-int front = -1;
-int end = -1;
+// struct thread_control_block *TCBTable[MAX_THREADS];
+// int front = -1;
+// int end = -1;
 
-// Enqueue function for Queue of TCB's
-void enQueue(struct thread_control_block *thread){
-	// If the number of threads is already 128, print error
-	if (end == MAX_THREADS -1)
-		printf("ERROR: Maximum number of threads reached");
-	// Set front to be 0 if first time queueing
-	if (front == -1)
-		front = 0;
-	TCBTable[++end] = thread;
-}
+// // Enqueue function for Queue of TCB's
+// void enQueue(struct thread_control_block *thread){
+// 	// If the number of threads is already 128, print error
+// 	if (end == MAX_THREADS -1)
+// 		printf("ERROR: Maximum number of threads reached");
+// 	// Set front to be 0 if first time queueing
+// 	if (front == -1)
+// 		front = 0;
+// 	TCBTable[++end] = thread;
+// }
 
-// Dequeue function for Queue of TCB's
-void deQueue(){
-	// If trying to remove from empty, print error
-	if (front == -1)
-		print("ERROR: No threads open\n");
-	free_thread(TCBTable[front]);
-	// If current is at the end, reinitialize all values to -1
-	if (++front > end){
-		front = -1;
-		end = -1;
-	}
-	
-}
+// // Dequeue function for Queue of TCB's
+// void deQueue(){
+// 	// If trying to remove from empty, print error
+// 	if (front == -1)
+// 		print("ERROR: No threads open\n");
+// 	free_thread(TCBTable[front]);
+// 	// If current is at the end, reinitialize all values to -1
+// 	if (++front > end){
+// 		front = -1;
+// 		end = -1;
+// 	}
+// }
 
 
 // // Function to free a thread
@@ -84,6 +83,7 @@ void deQueue(){
 // }
 
 // Global thread variables: TCB table with ALL current threads and the currently running thread
+
 struct TCBTable {
 	int size;
 	struct thread_control_block * currentThread;
@@ -92,7 +92,6 @@ struct TCBTable {
 
 struct TCBTable * TCB;
 void * stackToFree = NULL;
-int numThread;
 
 // SIGALRM handler that saves current context and moves onto the next function
 static void schedule(int signal)
@@ -103,43 +102,49 @@ static void schedule(int signal)
 		stackToFree = NULL;
 	}
 
-	// Use setjmp to update currently active thread's jmp_buf
-	setjmp(TCB->currentThread->currentContext);
+	// Use setjmp to update currently active thread's jmp_buf, if jumped to = no scheduling
+	if(setjmp(TCB->currentThread->currentContext) == 0){
 
-	// If current thread is done, then free the thread and move on
-	if(TCB->currentThread->status == TS_EXITED){
-		struct thread_control_block * current = TCB->currentThread;
+		// If current thread is done, then free the thread and move on
+		if(TCB->currentThread->status == TS_EXITED){
+			struct thread_control_block * current = TCB->currentThread;
 
-		stackToFree = TCB->currentThread->stackPtr;
+			stackToFree = TCB->currentThread->stackPtr;
 
-		// If there are more threads, set up the next thread. Otherwise, do nothing.
-		if (current->nextThread != NULL){
-			TCB->currentThread = current->nextThread;
-			TCB->currentThread->status = TS_RUNNING;
+			// If there are more threads, set up the next thread. Otherwise, do nothing.
+			if (current->nextThread != NULL){
+				TCB->currentThread = current->nextThread;
+				TCB->currentThread->status = TS_RUNNING;
+			}
+			
+			// Free the finished thread (don't need to free stack or context since those are freed in thread exit)
+			free(current->currentContext);
+			free(current);
+		}
+		else{
+			// Initialize timer: Send SIGALRM in 50ms
+			if (ualarm(SCHEDULER_INTERVAL_USECS, 0) < 0){
+				printf("ERROR: Timer not set\n");
+				exit(-1);
+			}
+
+			TCB->currentThread->status = TS_READY;
+
+			// If a next thread exists, set all the pointers and jump to new thread
+			if (TCB->currentThread->nextThread != NULL){
+				// Move from current thread to next thread and move current to last thread
+				TCB->lastThread->nextThread = TCB->currentThread;
+				TCB->lastThread = TCB->currentThread;
+				TCB->lastThread->nextThread = NULL;
+				TCB->currentThread = TCB->currentThread->nextThread;
+				TCB->currentThread->status = TS_RUNNING;
+				// Jump to the next thread
+				longjmp(TCB->currentThread->currentContext, 1);
+			}
+			// If there is no more next threads but the current thread is not done, just keep running
 		}
 		
-		// Free the finished thread (don't need to free stack or context since those are freed in thread exit)
-		free(current->currentContext);
-		free(current);
 	}
-	else{
-		TCB->currentThread->status = TS_READY;
-
-		// If a next thread exists, set all the pointers and jump to new thread
-		if (TCB->currentThread->nextThread != NULL){
-			// Move from current thread to next thread and move current to last thread
-			TCB->lastThread->nextThread = TCB->currentThread;
-			TCB->lastThread = TCB->currentThread;
-			TCB->lastThread->nextThread = NULL;
-			TCB->currentThread = TCB->currentThread->nextThread;
-			TCB->currentThread->status = TS_RUNNING;
-
-			// Jump to the next thread
-			longjmp(TCB->currentThread->currentContext, 1);
-		}
-		// If there is no more next threads but the current thread is not done, just keep running
-	}
-
 }
 
 /* TODO: do everything that is needed to initialize your scheduler. For example:
@@ -152,18 +157,15 @@ static void schedule(int signal)
 */
 static void scheduler_init()
 {
-
 	// Allocate memory for the TCB table with MAX_THREADS entries
 	TCB = (struct TCBTable *) malloc(sizeof(struct TCBTable) + sizeof(struct thread_control_block) * MAX_THREADS);
 	TCB->size = 0;
 	TCB->currentThread = (struct thread_control_block *) malloc(sizeof(struct thread_control_block));
 	TCB->currentThread->nextThread = NULL;
 	TCB->currentThread->stackPtr = NULL;
-	TCB->currentThread->nextThread->status = TS_RUNNING;
-	TCB->currentThread->tid = 0;
+	TCB->currentThread->status = TS_RUNNING;
+	TCB->currentThread->tid = TCB->size++;
 
-	// Set global value of numThread
-	numThread = 1;
 	
 	// Set signal handler to schedule
 	struct sigaction sigAlrmAction;
@@ -174,7 +176,7 @@ static void scheduler_init()
 	sigaction(SIGCHLD, &sigAlrmAction, NULL);
 	
 	// Initialize timer: Every 50ms sends SIGALRM
-	if (ualarm(SCHEDULER_INTERVAL_USECS, SCHEDULER_INTERVAL_USECS) < 0){
+	if (ualarm(SCHEDULER_INTERVAL_USECS, 0) < 0){
 		printf("ERROR: Timer not set\n");
 		exit(-1);
 	}
@@ -195,70 +197,38 @@ int pthread_create(
 		scheduler_init();
 	}
 
-
+	struct thread_control_block * newThread = (struct thread_control_block *) malloc(sizeof(struct thread_control_block));
+	newThread->nextThread = NULL;
+	newThread->status = TS_READY;
+	newThread->tid = TCB->size;
 
 	// Create the stack: Dynamically allocate memory
 	void * stackPtr =  malloc(THREAD_STACK_SIZE);
-	*(unsigned long *) (stackPtr + THREAD_STACK_SIZE - 8) = (unsigned long int) &pthread_exit;
+	unsigned long * SP = (unsigned long *) stackPtr + THREAD_STACK_SIZE - 8;
+	*SP = (unsigned long int) &pthread_exit;
+	newThread->stackPtr = stackPtr;
 	
+
 	//ptr mangle start_thunk and the pthread_exit thing
+	setjmp(newThread->currentContext);
 
-	jmp_buf threadBuf;
-	setjmp(threadBuf);
-	TCB->currentThread->currentContext.__jmpbuf[JB_R12] = 6;
-	RSP and pc use, and ptr to start routine
+	newThread->currentContext[0].__jmpbuf[JB_RSP] = ptr_mangle( (unsigned long) stackPtr + THREAD_STACK_SIZE - 8);
+	newThread->currentContext[0].__jmpbuf[JB_R12] = (unsigned long) start_routine;
+	newThread->currentContext[0].__jmpbuf[JB_R13] = (unsigned long) arg;
+	newThread->currentContext[0].__jmpbuf[JB_PC] = ptr_mangle( (unsigned long) start_thunk );
 
-	/* TODO: Return 0 on successful thread creation, non-zero for an error.
-	 *       Be sure to set *thread on success.
-	 * Hints:
-	 * The general purpose is to create a TCB:
-	 * - Create a stack.
-	 * - Assign the stack pointer in the thread's registers. Important: where
-	 *   within the stack should the stack pointer be? It may help to draw
-	 *   an empty stack diagram to answer that question.
-	 * - Assign the program counter in the thread's registers.
-	 * - Wait... HOW can you assign registers of that new stack? 
-	 *   1. call setjmp() to initialize a jmp_buf with your current thread
-	 *   2. modify the internal data in that jmp_buf to create a new thread environment
-	 *      env->__jmpbuf[JB_...] = ...
-	 *      See the additional note about registers below
-	 *   3. Later, when your scheduler runs, it will longjmp using your
-	 *      modified thread environment, which will apply all the changes
-	 *      you made here.
-	 * - Remember to set your new thread as TS_READY, but only  after you
-	 *   have initialized everything for the new thread.
-	 * - Optionally: run your scheduler immediately (can also wait for the
-	 *   next scheduling event).
-	 */
-	/*
-	 * Setting registers for a new thread:
-	 * When creating a new thread that will begin in start_routine, we
-	 * also need to ensure that `arg` is passed to the start_routine.
-	 * We cannot simply store `arg` in a register and set PC=start_routine.
-	 * This is because the AMD64 calling convention keeps the first arg in
-	 * the EDI register, which is not a register we control in jmp_buf.
-	 * We provide a start_thunk function that copies R13 to RDI then jumps
-	 * to R12, effectively calling function_at_R12(value_in_R13). So
-	 * you can call your start routine with the given argument by setting
-	 * your new thread's PC to be ptr_mangle(start_thunk), and properly
-	 * assigning R12 and R13.
-	 *
-	 * Don't forget to assign RSP too! Functions know where to
-	 * return after they finish based on the calling convention (AMD64 in
-	 * our case). The address to return to after finishing start_routine
-	 * should be the first thing you push on your stack.
-	 */
-	return -1;
+	*thread = newThread->tid;
+	return 0;
 }
 
 /* TODO: Exit the current thread instead of exiting the entire process.
-	 * Hints:
-	 * - Release all resources for the current thread. CAREFUL though.
-	 *   If you free() the currently-in-use stack then do something like
-	 *   call a function or add/remove variables from the stack, bad things
-	 *   can happen.
-	 * - Update the thread's status to indicate that it has exited
-	 */
+* Hints:
+* - Release all resources for the current thread. CAREFUL though.
+*   If you free() the currently-in-use stack then do something like
+*   call a function or add/remove variables from the stack, bad things
+*   can happen.
+* - Update the thread's status to indicate that it has exited
+*/
 void pthread_exit(void *value_ptr)
 {
 	// Set the current thread's status to exited
@@ -271,12 +241,12 @@ void pthread_exit(void *value_ptr)
 	exit(1);
 }
 
+/* TODO: Return the current thread instead of -1
+* Hint: this function can be implemented in one line, by returning
+* a specific variable instead of -1.
+*/
 pthread_t pthread_self(void)
 {
-
-	/* TODO: Return the current thread instead of -1
-	 * Hint: this function can be implemented in one line, by returning
-	 * a specific variable instead of -1.
-	 */
+	// Return tid of current thread
 	return TCB->currentThread->tid;
 }
