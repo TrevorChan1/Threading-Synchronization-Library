6aca6ee0772cd0a2c767f9e47872285038858d34
Trevor Chan <trevchan@bu.edu>
2023-02-24 18:11:41 -0500

    Trying to add just 1 ualarm with intervals

--- threads.c
+++ threads.c
@@ -63,11 +63,6 @@ void * stackToFree = NULL;
 // SIGALRM handler that saves current context and moves onto the next function
 static void schedule(int signal)
 {
-	// Initialize timer: Send SIGALRM in 50ms
-			if (ualarm(SCHEDULER_INTERVAL_USECS, 0) < 0){
-				printf("ERROR: Timer not set\n");
-				exit(-1);
-			}
 	// If a previous thread has exited, free the stack and set global stackToFree to NULL
 	if (stackToFree){
 		free(stackToFree);
@@ -89,10 +84,10 @@ static void schedule(int signal)
 				TCB->currentThread->status = TS_RUNNING;
 				free(current);
 				// Initialize timer: Send SIGALRM in 50ms
-				if (ualarm(SCHEDULER_INTERVAL_USECS, 0) < 0){
-					printf("ERROR: Timer not set\n");
-					exit(-1);
-				}
+				// if (ualarm(SCHEDULER_INTERVAL_USECS, 0) < 0){
+				// 	printf("ERROR: Timer not set\n");
+				// 	exit(-1);
+				// }
 				// Go to the next thread to be run
 				longjmp(TCB->currentThread->currentContext, 1);
 			}
@@ -102,10 +97,10 @@ static void schedule(int signal)
 		}
 		else{
 			// Initialize timer: Send SIGALRM in 50ms
-			if (ualarm(SCHEDULER_INTERVAL_USECS, 0) < 0){
-				printf("ERROR: Timer not set\n");
-				exit(-1);
-			}
+			// if (ualarm(SCHEDULER_INTERVAL_USECS, 0) < 0){
+			// 	printf("ERROR: Timer not set\n");
+			// 	exit(-1);
+			// }
 
 			TCB->currentThread->status = TS_READY;
 
@@ -156,7 +151,7 @@ static void scheduler_init()
 	sigaction(SIGALRM, &sigAlrmAction, NULL);
 	
 	// Initialize timer: Every 50ms sends SIGALRM
-	if (ualarm(SCHEDULER_INTERVAL_USECS, 0) < 0){
+	if (ualarm(SCHEDULER_INTERVAL_USECS, SCHEDULER_INTERVAL_USECS) < 0){
 		printf("ERROR: Timer not set\n");
 		exit(-1);
 	}
@@ -221,7 +216,7 @@ int pthread_create(
 void pthread_exit(void *value_ptr)
 {
 	// Cancel any current alarms
-	ualarm(0,0);
+	// ualarm(0,0);
 	// Set the current thread's status to exited
 	TCB->currentThread->status = TS_EXITED;
 
