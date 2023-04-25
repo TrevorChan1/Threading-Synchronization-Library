b56be877213ce81cce5995d3fdadc4be4c7b7de7
Trevor Chan <trevchan@bu.edu>
2023-04-25 19:40:42 -0400

    Added AUstin tests

---
+++ tests/test2.c
@@ -0,0 +1,41 @@
+#include "../threads.c"
+#include <stdio.h>
+#include <sys/types.h>
+
+pthread_mutex_t mutex;
+pthread_barrier_t barrier;
+
+void *routine1(void *arg) {
+	for (unsigned int i = 0; i < 1000000000; ++i) {
+		if (i % 100000000 == 0) {
+			printf("%lu says %u\n", pthread_self(), i);
+			printf("%u\n", pthread_barrier_wait(&barrier));
+		}
+	}
+	return NULL;
+}
+
+void *routine2(void *arg) {
+	for (unsigned int i = 0; i < 1000000000; ++i) {
+		if (i % 100000000 == 0) {
+			printf("%lu says %u\n", pthread_self(), i);
+			printf("%u\n", pthread_barrier_wait(&barrier));
+		}
+	}
+	return NULL;
+}
+
+int main() {
+
+	pthread_t tid[2];
+
+	pthread_create(tid, NULL, &routine1, NULL);
+	pthread_create(tid+1, NULL, &routine2, NULL);
+	pthread_barrier_init(&barrier, NULL, 2);
+
+	pthread_exit(NULL);
+
+	printf("this should not be printed\n");
+
+	return 0;
+}
---
+++ tests/test6.c
@@ -0,0 +1,49 @@
+#include "../threads.c"
+#include <stdio.h>
+#include <sys/types.h>
+
+pthread_mutex_t mutex;
+
+void *add(void *arg) {
+	pthread_mutex_lock(&mutex);
+	int *money = (int *) arg;
+	for (unsigned int i = 0; i < 1000000000; ++i) {
+		if (i % 10000000 == 0) {
+			*money += 1;
+			printf("add: money = %u\n", *money);
+		}
+	}
+	pthread_mutex_unlock(&mutex);
+	return NULL;
+}
+
+void *sub(void *arg) {
+	pthread_mutex_lock(&mutex);
+	int *money = (int *) arg;
+	for (unsigned int i = 0; i < 1000000000; ++i) {
+		if (i % 10000000 == 0) {
+			*money -= 1;
+			printf("sub: money = %u\n", *money);
+		}
+	}
+	pthread_mutex_unlock(&mutex);
+	return NULL;
+}
+
+int main() {
+
+	pthread_t tid[2];
+
+	pthread_mutex_init(&mutex, NULL);
+
+	int money = 0;
+
+	pthread_create(tid, NULL, &add, &money);
+	pthread_create(tid+1, NULL, &sub, &money);
+
+	pthread_exit(NULL);
+
+	printf("this line should not be printed\n");
+
+	return 0;
+}
